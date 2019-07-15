92494544fa1d206ab50e80b0fcb686d601a3fed0
Guilherme Abel <39908676+guilhermeabel@users.noreply.github.com>
2019-07-15 02:04:19 -0300

    Novas cores

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,10 +25,10 @@
         <!-- Nav -->
         <nav id="nav">
             <ul>
-                <li><a href="#intro" class="active">Introduction</a></li>
-                <li><a href="#first">First Section</a></li>
-                <li><a href="#second">Second Section</a></li>
-                <li><a href="#cta">Get Started</a></li>
+                <li><a href="#intro" class="active">Sobre</a></li>
+                <li><a href="#first">Projeto</a></li>
+                <li><a href="#second">Contato</a></li>
+                <li><a href="#cta">Acesso</a></li>
             </ul>
         </nav>
         <!-- HOME
