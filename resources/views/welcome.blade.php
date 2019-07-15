6f21f225d7b62fa9828d38810c092bf52866f19e
Guilherme Abel <39908676+guilhermeabel@users.noreply.github.com>
2019-07-15 01:36:03 -0300

    Novo layout
    Novo template implementado (js, css, html)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,20 +2,36 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
+    <title>WebSim</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <!-- Styles -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="css/style-1.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" />
+    <noscript>
+        <link rel="stylesheet" href="css/noscript.css" /></noscript>
 </head>
 
-<body>
-    <!-- HOME -->
+<body class="is-preload">
+    <!-- Wrapper -->
+    <div id="wrapper">
+        <!-- Header -->
+        <header id="header" class="alt">
+            <span class="logo"><img src="image/logo.png" alt="" /></span>
+            <h1><b>WebSim</b></h1>
+            <p>A solução perfeita para simulações online <br />
+        </header>
+        <!-- Nav -->
+        <nav id="nav">
+            <ul>
+                <li><a href="#intro" class="active">Introduction</a></li>
+                <li><a href="#first">First Section</a></li>
+                <li><a href="#second">Second Section</a></li>
+                <li><a href="#cta">Get Started</a></li>
+            </ul>
+        </nav>
+        <!-- HOME
     <div class="home flex-center position-ref full-height">
         @if (Route::has('login'))
         <div class="top-right links">
@@ -29,93 +45,167 @@
             @endif
             @endauth
         </div>
-        @endif
+        @endif -->
 
-        <div class="content">
-            <div class="title m-b-md">
-                >/ WebSim
-            </div>
+        <!-- Main -->
+        <div id="main">
 
-            <div class="links">
-                <a href="https://laravel.com/docs">Links</a>
-                <a href="https://laracasts.com">úteis</a>
-                <a href="https://laravel-news.com">Sobre</a>
-                <a href="https://blog.laravel.com">O projeto</a>
-                <a href="https://nova.laravel.com">OCA</a>
-                <a href="https://github.com/laravel/laravel">Contato</a>
-            </div>
+            <!-- Introduction -->
+            <section id="intro" class="main">
+                <div class="spotlight">
+                    <div class="content">
+                        <header class="major">
+                            <h2>Ipsum sed adipiscing</h2>
+                        </header>
+                        <p>Sed lorem ipsum dolor sit amet nullam consequat feugiat consequat magna
+                            adipiscing magna etiam amet veroeros. Lorem ipsum dolor tempus sit cursus.
+                            Tempus nisl et nullam lorem ipsum dolor sit amet aliquam.</p>
+                        <ul class="actions">
+                            <li><a href="generic.html" class="button">Learn More</a></li>
+                        </ul>
+                    </div>
+                    <span class="image"><img src="image/pic01.jpg" alt="" /></span>
+                </div>
+            </section>
+
+            <!-- First Section -->
+            <section id="first" class="main special">
+                <header class="major">
+                    <h2>Magna veroeros</h2>
+                </header>
+                <ul class="features">
+                    <li>
+                        <span class="icon solid major style1 fa-code"></span>
+                        <h3>Ipsum consequat</h3>
+                        <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed
+                            consequat.</p>
+                    </li>
+                    <li>
+                        <span class="icon major style3 fa-copy"></span>
+                        <h3>Amed sed feugiat</h3>
+                        <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed
+                            consequat.</p>
+                    </li>
+                    <li>
+                        <span class="icon major style5 fa-gem"></span>
+                        <h3>Dolor nullam</h3>
+                        <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed
+                            consequat.</p>
+                    </li>
+                </ul>
+                <footer class="major">
+                    <ul class="actions special">
+                        <li><a href="generic.html" class="button">Learn More</a></li>
+                    </ul>
+                </footer>
+            </section>
+
+            <!-- Second Section -->
+            <section id="second" class="main special">
+                <header class="major">
+                    <h2>Ipsum consequat</h2>
+                    <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
+                        posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
+                </header>
+                <ul class="statistics">
+                    <li class="style1">
+                        <span class="icon solid fa-code-branch"></span>
+                        <strong>5,120</strong> Etiam
+                    </li>
+                    <li class="style2">
+                        <span class="icon fa-folder-open"></span>
+                        <strong>8,192</strong> Magna
+                    </li>
+                    <li class="style3">
+                        <span class="icon solid fa-signal"></span>
+                        <strong>2,048</strong> Tempus
+                    </li>
+                    <li class="style4">
+                        <span class="icon solid fa-laptop"></span>
+                        <strong>4,096</strong> Aliquam
+                    </li>
+                    <li class="style5">
+                        <span class="icon fa-gem"></span>
+                        <strong>1,024</strong> Nullam
+                    </li>
+                </ul>
+                <p class="content">Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus
+                    hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus.
+                    Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed
+                    eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur
+                    vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis
+                    porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam
+                    porta. Cras eu ornare dui curabitur lacinia.</p>
+                <footer class="major">
+                    <ul class="actions special">
+                        <li><a href="generic.html" class="button">Learn More</a></li>
+                    </ul>
+                </footer>
+            </section>
+
+            <!-- Get Started -->
+            <section id="cta" class="main special">
+                <header class="major">
+                    <h2>Congue imperdiet</h2>
+                    <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
+                        posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
+                </header>
+                <footer class="major">
+                    <ul class="actions special">
+                        <li><a href="generic.html" class="button primary">Get Started</a></li>
+                        <li><a href="generic.html" class="button">Learn More</a></li>
+                    </ul>
+                </footer>
+            </section>
+
         </div>
+
+        <!-- Footer -->
+        <footer id="footer">
+            <section>
+                <h2>Aliquam sed mauris</h2>
+                <p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam
+                    dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat
+                    egestas velit, vitae tincidunt odio.</p>
+                <ul class="actions">
+                    <li><a href="generic.html" class="button">Learn More</a></li>
+                </ul>
+            </section>
+            <section>
+                <h2>Etiam feugiat</h2>
+                <dl class="alt">
+                    <dt>Address</dt>
+                    <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
+                    <dt>Phone</dt>
+                    <dd>(000) 000-0000 x 0000</dd>
+                    <dt>Email</dt>
+                    <dd><a href="#">information@untitled.tld</a></dd>
+                </dl>
+                <ul class="icons">
+                    <li><a href="#" class="icon brands fa-twitter alt"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
+                    <li><a href="#" class="icon brands fa-github alt"><span class="label">GitHub</span></a></li>
+                    <li><a href="#" class="icon brands fa-dribbble alt"><span class="label">Dribbble</span></a></li>
+                </ul>
+            </section>
+            <p class="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+        </footer>
+
     </div>
-    <!-- ABOUT -->
-    <!-- <div class="about flex-center position-ref full-height">
-        <div class="content">
-            <div class="card-deck m-5">
-                <div class="card bg-dark-1">
-                    <img src="image/example.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card bg-dark-1">
-                    <img src="image/example.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional
-                            content.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card bg-dark-1">
-                    <img src="image/example.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This card has even longer content than the first to show that equal
-                            height action.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-     <!-- CONTACT -->
-    <!-- <div class="about flex-center position-ref full-height">
-        <div class="content">
-            <div class="card-deck m-5">
-                <div class="card bg-dark-1">
-                    <img src="image/example.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card bg-dark-1">
-                    <img src="image/example.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional
-                            content.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card bg-dark-1">
-                    <img src="image/example.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This card has even longer content than the first to show that equal
-                            height action.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
+    <!-- Scripts -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.scrollex.min.js"></script>
+    <script src="js/jquery.scrolly.min.js"></script>
+    <script src="js/browser.min.js"></script>
+    <script src="js/breakpoints.min.js"></script>
+    <script src="js/util.js"></script>
+    <script src="js/main.js"></script>
+    <!--
+        <div>
+        Icons made by <a href="https://www.freepik.com/?__hstc=57440181.d13556a53a41a931131ba01ee808620f.1562787601055.1562787601055.1562787601055.1&__hssc=57440181.7.1562787601056&__hsfp=3160667711" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"
+        title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"
+        title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    -->
 </body>
-
 </html>
